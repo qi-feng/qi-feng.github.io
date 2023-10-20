d52fe1d9ab9fb3a071402e0535c6b8338b8da95a
Qi Feng <sely520@gmail.com>
2023-10-20 10:44:53 -0700

    Update

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 
 export const SITE_TITLE = 'Qi Feng';
 //export const SITE_SUBTITLE = 'Assistant Professor, University of Utah';
-export const SITE_SUBTITLE = 'Joining University of Utah in January 2024';
+export const SITE_SUBTITLE = 'Joining the University of Utah in January 2024';
 export const SITE_DESCRIPTION = 'Welcome to our group website!';
 export const PROFILE_PIC = '../public/QiFengPic.jpg';
 
